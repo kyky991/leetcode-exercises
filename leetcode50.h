15bd605b4a4f36f1e134da2170d491583243b5bd
cyunzing <cyunzing@gmail.com>
2018-05-30 23:46:46 +0800

    4,5

--- leetcode50.h
+++ leetcode50.h
@@ -67,7 +67,75 @@ int lengthOfLongestSubstring(string s)
 //4. Median of Two Sorted Arrays
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
 {
+    int m = nums1.size();
+    int n = nums2.size();
+    if (m > n)
+        return findMedianSortedArrays(nums2, nums1);
 
+    int minI = 0, maxI = m;
+    int half = (m + n + 1) / 2;
+    int maxL, minR;
+
+    while (minI <= maxI) {
+        int i = (minI + maxI) / 2;
+        int j = half - i;
+        if (i < m && nums2[j - 1] > nums1[i])
+            minI = i + 1;
+        else if (i > 0 && nums1[i - 1] > nums2[j])
+            maxI = i - 1;
+        else {
+            if (i == 0)
+                maxL = nums2[j - 1];
+            else if (j == 0)
+                maxL = nums1[i - 1];
+            else
+                maxL = max(nums1[i - 1], nums2[j - 1]);
+
+            if ((m + n) % 2 == 1)
+                return maxL;
+
+            if (i == m)
+                minR = nums2[j];
+            else if (j == n)
+                minR = nums1[i];
+            else
+                minR = min(nums1[i], nums2[j]);
+
+            return (maxL + minR) / 2.0;
+        }
+    }
+}
+
+//5. Longest Palindromic Substring
+string longestPalindrome(string s)
+{
+    if (s.empty())
+        return "";
+    if (s.size() == 1)
+        return s;
+
+    int i = 0, start = 0, maxLen = 1;
+    while (i < s.size()) {
+        if (s.size() - i <= maxLen / 2)
+            break;
+
+        int j = i, k = i;
+        while (k < s.size() - 1 && s[k + 1] == s[k]) {
+            k++;
+        }
+        i = k + 1;
+        while (k < s.size() - 1 && j > 0 && s[k + 1] == s[j - 1]) {
+            k++;
+            j--;
+        }
+        int len = k - j + 1;
+        if (len > maxLen) {
+            start = j;
+            maxLen = len;
+        }
+    }
+
+    return s.substr(start, maxLen);
 }
 #endif // LEETCODE50
 
