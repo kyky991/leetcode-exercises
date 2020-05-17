8cf38b6ee9c86ae19a4d485ba7333d35426364f1
Zing <cyunzing@gmail.com>
2020-05-18 00:20:50 +0800

    Intersection of Two Linked Lists Remove Nth Node From End of List Reverse Linked List Remove Linked List Elements

--- java/src/com/zing/leetcode/explore/learn/linkedlist/LinkedListSolution.java
+++ java/src/com/zing/leetcode/explore/learn/linkedlist/LinkedListSolution.java
@@ -48,4 +48,118 @@ public class LinkedListSolution {
         }
         return null;
     }
+
+    /**
+     * Intersection of Two Linked Lists
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int sizeA = 0, sizeB = 0;
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while (curA != null || curB != null) {
+            if (curA != null) {
+                sizeA++;
+                curA = curA.next;
+            }
+            if (curB != null) {
+                sizeB++;
+                curB = curB.next;
+            }
+        }
+        int offset = Math.abs(sizeA - sizeB);
+        ListNode node1 = sizeA >= sizeB ? headA : headB;
+        ListNode cur2 = sizeA >= sizeB ? headB : headA;
+        ListNode cur1 = node1;
+        while (offset > 0) {
+            cur1 = cur1.next;
+            offset--;
+        }
+        while (cur1 != null && cur2 != null) {
+            if (cur1 == cur2) {
+                return cur1;
+            }
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return null;
+    }
+
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while (curA != curB) {
+            curA = curA == null ? headB : curA.next;
+            curB = curB == null ? headA : curB.next;
+        }
+        return curA;
+    }
+
+    /**
+     * Remove Nth Node From End of List
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        start.next = head;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+
+    /**
+     * Reverse Linked List
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode start = new ListNode(0);
+        start.next = head;
+
+        while (head != null && head.next != null) {
+            ListNode next = head.next;
+            head.next = next.next;
+            next.next = start.next;
+            start.next = next;
+        }
+        return start.next;
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    /**
+     * Remove Linked List Elements
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode start = new ListNode(0);
+        start.next = head;
+
+        ListNode cur = start;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return start.next;
+    }
 }
