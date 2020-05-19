4510a0f35253bbbdaaf8039791270d6563acb228
Zing <cyunzing@gmail.com>
2020-05-19 23:51:13 +0800

    Palindrome Linked List, Merge Two Sorted Lists, Add Two Numbers

--- java/src/com/zing/leetcode/explore/learn/linkedlist/LinkedListSolution.java
+++ java/src/com/zing/leetcode/explore/learn/linkedlist/LinkedListSolution.java
@@ -182,4 +182,116 @@ public class LinkedListSolution {
         return head;
     }
 
+    /**
+     * Palindrome Linked List
+     */
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        slow = reverseList2(slow);
+        fast = head;
+        while (slow != null && fast != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    /**
+     * Merge Two Sorted Lists
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (l1 != null || l2 != null) {
+            if (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                } else {
+                    cur.next = l2;
+                    l2 = l2.next;
+                }
+            } else if (l1 != null) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        return head.next;
+    }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return head.next;
+    }
+
+    public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists3(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists3(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /**
+     * Add Two Numbers
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode newHead = new ListNode(0);
+        ListNode cur = newHead;
+        int add = 0;
+        while (l1 != null || l2 != null || add > 0) {
+            int sum = add;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            add = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+        }
+
+        return newHead.next;
+    }
 }
