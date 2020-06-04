8441febf524b41521e1e3b4346acb713a45f76ae
Zing <cyunzing@gmail.com>
2020-06-04 10:36:33 +0800

    bfs

--- java/src/com/zing/basic/TestAlgorithm.java
+++ java/src/com/zing/basic/TestAlgorithm.java
@@ -1,9 +1,6 @@
 package com.zing.basic;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Zing
@@ -11,7 +8,7 @@ import java.util.List;
  */
 public class TestAlgorithm {
 
-    public static int fib(int n) {
+    public int fib(int n) {
         if (n <= 0) {
             return n;
         }
@@ -19,7 +16,7 @@ public class TestAlgorithm {
         return fib(memo, n);
     }
 
-    private static int fib(int[] memo, int n) {
+    private int fib(int[] memo, int n) {
         if (n == 1 || n == 2) {
             return 1;
         }
@@ -30,7 +27,7 @@ public class TestAlgorithm {
         return memo[n];
     }
 
-    public static int dpFib(int n) {
+    public int dpFib(int n) {
         int[] dp = new int[n + 1];
         dp[1] = dp[2] = 1;
         for (int i = 3; i <= n; i++) {
@@ -39,7 +36,7 @@ public class TestAlgorithm {
         return dp[n];
     }
 
-    public static int dpFib2(int n) {
+    public int dpFib2(int n) {
         if (n == 1 || n == 2) {
             return 1;
         }
@@ -52,7 +49,7 @@ public class TestAlgorithm {
         return cur;
     }
 
-    public static int coinChange(int[] coins, int amount) {
+    public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
 
@@ -69,14 +66,14 @@ public class TestAlgorithm {
         return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 
-    public static List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         LinkedList<Integer> track = new LinkedList<>();
         backtrack(nums, track, res);
         return res;
     }
 
-    private static void backtrack(int[] nums, LinkedList<Integer> track, List<List<Integer>> res) {
+    private void backtrack(int[] nums, LinkedList<Integer> track, List<List<Integer>> res) {
         if (track.size() == nums.length) {
             res.add(new LinkedList<>(track));
             return;
@@ -92,7 +89,7 @@ public class TestAlgorithm {
         }
     }
 
-    public static List<List<String>> solveNQueens(int n) {
+    public List<List<String>> solveNQueens(int n) {
         List<List<String>> res = new ArrayList<>();
         char[][] board = new char[n][n];
         for (int i = 0; i < n; i++) {
@@ -104,7 +101,7 @@ public class TestAlgorithm {
         return res;
     }
 
-    private static void backtrack(char[][] board, int row, List<List<String>> res) {
+    private void backtrack(char[][] board, int row, List<List<String>> res) {
         // 路径：board 中小于 row 的那些行都已经成功放置了皇后
         // 选择列表：第 row 行的所有列都是放置皇后的选择
         // 结束条件：row 超过 board 的最后一行
@@ -126,7 +123,7 @@ public class TestAlgorithm {
         }
     }
 
-    private static boolean isValid(char[][] board, int row, int col) {
+    private boolean isValid(char[][] board, int row, int col) {
         //check if the column had a queen before.
         for (int i = 0; i < row; i++) {
             if (board[i][col] == 'Q') {
@@ -146,13 +143,146 @@ public class TestAlgorithm {
         return true;
     }
 
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+    public int openLock(String[] deadends, String target) {
+        Set<String> deads = new HashSet<>();
+        for (String s : deadends) {
+            deads.add(s);
+        }
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("0000");
+        visited.add("0000");
+
+        int step = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String cur = queue.poll();
+                if (deads.contains(cur)) {
+                    continue;
+                }
+                if (cur.equals(target)) {
+                    return step;
+                }
+
+                for (int j = 0; j < 4; j++) {
+                    String up = plusOne(cur, j);
+                    if (!visited.contains(up)) {
+                        queue.offer(up);
+                        visited.add(up);
+                    }
+                    String down = minusOne(cur, j);
+                    if (!visited.contains(down)) {
+                        queue.offer(down);
+                        visited.add(down);
+                    }
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+
+    private String plusOne(String cur, int i) {
+        char[] chars = cur.toCharArray();
+        if (chars[i] == '9') {
+            chars[i] = '0';
+        } else {
+            chars[i] += 1;
+        }
+        return String.valueOf(chars);
+    }
+
+    private String minusOne(String cur, int i) {
+        char[] chars = cur.toCharArray();
+        if (chars[i] == '0') {
+            chars[i] = '9';
+        } else {
+            chars[i] -= 1;
+        }
+        return String.valueOf(chars);
+    }
+
+    public int openLock2(String[] deadends, String target) {
+        Set<String> deads = new HashSet<>();
+        for (String s : deadends) {
+            deads.add(s);
+        }
+        Set<String> visited = new HashSet<>();
+        Set<String> q1 = new HashSet<>();
+        Set<String> q2 = new HashSet<>();
+        q1.add("0000");
+        q2.add(target);
+
+        int step = 0;
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            Set<String> tmp = new HashSet<>();
+            for (String cur : q1) {
+                if (deads.contains(cur)) {
+                    continue;
+                }
+                if (q2.contains(cur)) {
+                    return step;
+                }
+
+                visited.add(cur);
+
+                for (int j = 0; j < 4; j++) {
+                    String up = plusOne(cur, j);
+                    if (!visited.contains(up)) {
+                        tmp.add(up);
+                    }
+                    String down = minusOne(cur, j);
+                    if (!visited.contains(down)) {
+                        tmp.add(down);
+                    }
+                }
+            }
+            step++;
+
+            q1 = q2;
+            q2 = tmp;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(fib(5));
-        System.out.println(dpFib(5));
-        System.out.println(dpFib2(5));
-        System.out.println(coinChange(new int[]{1, 2, 5}, 11));
-        System.out.println(permute(new int[]{1, 2, 5}));
-        System.out.println(solveNQueens(4));
+        TestAlgorithm algorithm = new TestAlgorithm();
+        System.out.println(algorithm.fib(5));
+        System.out.println(algorithm.dpFib(5));
+        System.out.println(algorithm.dpFib2(5));
+        System.out.println(algorithm.coinChange(new int[]{1, 2, 5}, 11));
+        System.out.println(algorithm.permute(new int[]{1, 2, 5}));
+        System.out.println(algorithm.solveNQueens(4));
     }
 
 }
